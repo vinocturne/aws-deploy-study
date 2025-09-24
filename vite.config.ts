06941bdc8715831ea081228bb5b216f0ff0e9552
vinocturne <vinocturne@naver.com>
2025-09-24 14:51:20 +0900

    fix: 빌드 설정 및 문법 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
-})
+	plugins: [react(), TanStackRouterVite()],
+	build: {
+		target: "es2018", // Optional Chaining 지원하지 않는 버전
+		minify: "esbuild",
+	},
+});
