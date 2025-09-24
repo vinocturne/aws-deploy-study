4533e3e2241ee4a7bb394d43b56014cf0c441c97
vinocturne <vinocturne@naver.com>
2025-09-24 15:59:24 +0900

    fix : 필요없는 import 삭제

--- src/routes/noSnap.tsx
+++ src/routes/noSnap.tsx
@@ -1,12 +1,4 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-import { Button } from "../components/ui/Button";
-import { Input } from "../components/ui/Input";
-import { useState } from "react";
-import supabase from "../utils/supabase";
-import { Helmet } from "react-helmet-async";
 
 export const Route = createFileRoute("/noSnap")({
 	component: NoSnap,
